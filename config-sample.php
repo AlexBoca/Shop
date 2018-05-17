06f532e07803b177b94473488856e1318ea8f947
HowTo <alex.boca1992@gmail.com>
2018-05-17 11:59:08 +0300

    edit gtiignore & .idea & config

--- config-sample.php
+++ config-sample.php
@@ -1,11 +1,11 @@
 <?php
-define('SITE_NAME', 'shop');
-define('BASE_URL', 'http://localhost/' . SITE_NAME);
-define('ROOT', __DIR__ . '/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('SITE_NAME', '');
+define('BASE_URL', '');
+define('ROOT',   '');
+define('DB_HOST', '');
+define('DB_USER', '');
 define('DB_PASS', '');
-define('DB_NAME', 'shop');
-define('ADMIN_USERNAME', 'admin');
-define('ADMIN_PASSWORD', 'admin');
-define('MANAGER_EMAIL', 'admin@shop.com');
+define('DB_NAME', '');
+define('ADMIN_USERNAME', '');
+define('ADMIN_PASSWORD', '');
+define('MANAGER_EMAIL', '');
